e1461932b338c5a32f922381bb113aa32862b346
Vasili579 <vasialag51@gmail.com>
2025-12-18 00:02:28 +0300

    Углубление в Playwright

--- playwright_errors.py
+++ playwright_errors.py
@@ -8,34 +8,44 @@ with sync_playwright() as playwright:
 
     page.goto(link)
 
-    """
-    AssertionError: Locator expected to be visible
-    Actual value: None                                                                                                                                                                                      
-    Error: element(s) not found                                                                                                                                                                             
-    Call log:                                                                                                                                                                                               
-        - Expect "to_be_visible" with timeout 5000ms                                                                                                                                                          
-        - waiting for locator("#unknown")  
-    """
+    # Пытаемся проверить, что несуществующий локатор виден на странице:
     # unknown = page.locator('#unknown')
     # expect(unknown).to_be_visible()
+    """
+        AssertionError: Locator expected to be visible
+        Actual value: None                                                                                                                                                                                      
+        Error: element(s) not found                                                                                                                                                                             
+        Call log:                                                                                                                                                                                               
+            - Expect "to_be_visible" with timeout 5000ms                                                                                                                                                          
+            - waiting for locator("#unknown")  
+        """
 
-    """
-    playwright._impl._errors.TimeoutError: Locator.fill: Timeout 30000ms exceeded.
-    """
+    # Пытаемся ввести текст в кнопку Login:
     # login_button = page.get_by_test_id('login-page-login-button')
     # login_button.fill('unknown')
+    """
+        playwright._impl._errors.TimeoutError: Locator.fill: Timeout 30000ms exceeded.
+        attempting fill action
+        -   waiting for element to be visible, enabled and editable
+        -   element is not enabled
+    """
 
 
-    """
-    playwright._impl._errors.Error: Page.evaluate: TypeError: Cannot set properties of null (setting 'textContent')
-    
-    Тайтл не успел загрузиться. В page.goto() добавить wait_until="networkidle" -> page.goto(link, wait_until="networkidle")
-    """
+    # Пытаемся изменить текст заголовка
     page.evaluate(
         """
         const title = document.getElementById('authentication-ui-course-title-text')
         title.textContent = 'New text'
         """
     )
+    """
+        playwright._impl._errors.Error: Page.evaluate: TypeError: Cannot set properties of null (setting 'textContent')
+            at eval (eval at evaluate (:234:30), <anonymous>:2:23)
+            at eval (<anonymous>)
+            at UtilityScript.evaluate (<anonymous>:234:30)
+            at UtilityScript.<anonymous> (<anonymous>:1:44)
+
+        Тайтл не успел загрузиться. В page.goto() добавить wait_until="networkidle" -> page.goto(link, wait_until="networkidle")
+    """
 
     page.wait_for_timeout(3000)
